058c6aecdefd9579b9c286daab879996eca16cf6
BhaveshDevCH <bhavesh.cyberhawkz@gmail.com>
2022-06-11 20:20:44 +0530

    de

--- cyber/settings.py
+++ cyber/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'l61o_e7uq*m@zs^t)$0g3-9ux8q(m(s*f1r7hg+4@l8gn_^ii4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
